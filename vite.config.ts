5d5052d3b3dde999e9bfcdad1d50211433c3fdb8
Jayden Lin <jaydenl@andrew.cmu.edu>
2025-11-09 22:42:45 -0500

    fixed again?

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/CMUPortfolio/' : '/',
+  base: mode === 'production' ? '/CMUPortfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
